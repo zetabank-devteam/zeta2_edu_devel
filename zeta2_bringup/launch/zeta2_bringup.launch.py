1ef8734a856ce4b78750f220f39820dc26f3b433
lolddohaja <82866376+lolddohaja@users.noreply.github.com>
2024-02-01 10:59:02 +0900

    update tf

--- zeta2_bringup/launch/zeta2_bringup.launch.py
+++ zeta2_bringup/launch/zeta2_bringup.launch.py
@@ -1,11 +1,17 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.conditions import UnlessCondition
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+import launch_ros
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
 
 def generate_launch_description():
+    zeta2_bringup_pkg = launch_ros.substitutions.FindPackageShare(package='zeta2_bringup').find('zeta2_bringup')
+    state_publisher_laucnh_file_path = os.path.join(zeta2_bringup_pkg, 'launch/zeta2_state_publisher.launch.py')
+
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_zeta_if',
@@ -66,5 +72,9 @@ def generate_launch_description():
                         ],
             output='screen',
             condition=IfCondition(LaunchConfiguration('use_odometry'))
-        )
+        ),
+        # Include the state publisher launch file
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(state_publisher_laucnh_file_path)
+        ),
     ])
